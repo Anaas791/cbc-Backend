30a23270e9927ce88dff8ad1ff2e8069b8a20841
Anaas <mhdanaas4@gmail.com>
2025-04-26 11:06:57 +0530

    product.js updating

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,33 +1,36 @@
 import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
-export function getProducts(req,res){
-    Product.find().then(
-        (data)=>{
-            res.json(data)
-        }
-    )
+export async function getProducts(req,res){
+   try{
+    if(isAdmin(req)){
+        const products = await Product.find()
+        res.json(products)
+    }else{
+        const products = await Product.find({isAvailable : true})
+        res.json(products)
+    }
+
+   }catch(err){
+    res.json({
+        message : "Failed to get product",
+        error : err
+    })
+   }
 }
 
 export function saveProducts(req,res){
 
-    if(req.user == null){
+    if(!isAdmin(res)){
         res.status(403).json({
-            message : "Unautherized"
+            message : "You are not autherized to add product"
         })
+        return
     }
 
-    if(req.user.role != "admin"){
-        res.status(403).json({
-            message : "unautherized you need to an admin"
-        })
-    }
-    
-
-    const product = new Product({
-        name: req.body.name,
-        price : req.body.price,
-        description : req.body.description
-    })
+    const product = new Product(
+        req.body
+    )
 
     product.save().then(()=>{
         res.json({
@@ -40,4 +43,89 @@ export function saveProducts(req,res){
     })
 }
 
-//test the commit
+export async function deleteProduct(req,res){
+
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message : "You are not autherized to delete product"
+        })
+        return
+    }
+    try{
+        await Product.deleteOne({productId : req.params.productId})
+
+        res.json({
+            message : "Product deleted successfully"
+        })
+    }catch(err){
+        res.status(500).json({
+            message : "Failed to delete product",
+            error : err
+        })
+    }
+}
+
+export async function updateProduct(req,res){
+
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message : "Your noty autherized to update product"
+        })
+        return
+    }
+
+    const productId = req.params.productId
+    const updatingData = req.body
+
+    try{
+        await Product.updateOne(
+            {productId : productId},
+            updatingData
+        )
+
+        res.json(
+            {
+                message : "Product update successfully"
+            }
+        )
+    }catch(err){
+        res.status(500).json({
+            message : "internal server error",
+            error : err
+        })
+    }
+}
+
+export async function getProductById(req,res){
+    const productId = req.params.productId
+
+    try{
+        const product = await Product.findOne(
+            {productId : productId}
+        )
+
+        if(product == null){
+            res.status(404).json({
+                message : "product not found"
+            })
+            return
+        }
+        if(product.isAvailable){
+            res.json(product)
+        }else{
+            if(!isAdmin(req)){
+                res.status(404).json({
+                    message : "product not found"
+                })
+                return
+            }else{
+                res.json(product)
+            }
+        }
+    }catch(err){
+        res.status(500).json({
+            message : "internal server error",
+            error : err
+        })
+    }
+}
--- router/productRouter.js
+++ router/productRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 
-import {getProducts,saveProducts} from "../controllers/productController.js";
+import {deleteProduct,getProducts,saveProducts,getProductById,updateProduct} from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/",getProducts);
-
 productRouter.post("/",saveProducts);
+productRouter.delete("/:productId",deleteProduct);
+productRouter.put("/:productId",updateProduct);
+productRouter.get("/:productId",getProductById)
 
 export default productRouter;
